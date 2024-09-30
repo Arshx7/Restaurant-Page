fe7f6bc4b88878ac759ebe89d107b7218487c2d9
Arshakh Haneen <arshx77@gmail.com>
2024-09-30 13:27:36 +0530

    Add initial files

---
+++ src/homePage.js
@@ -0,0 +1,11 @@
+export function home() {
+    const content = document.querySelector("#content")
+    const img = document.createElement("img");
+    const heading1 = document.createElement("h1");
+    const paragraph = document.createElement("p");
+
+    content.appendChild(img);
+    content.appendChild(heading1);
+    content.appendChild(paragraph);
+   
+}
